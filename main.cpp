9fbdbf1bc97abcd640e3cd8f7368dae909d86a9f
许继元 <490601115@qq.com>
2021-01-06 21:10:10 +0800

    feat: elevator simulation test

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/**
+ * @filename main.cpp
+ * @description Elevator Simulation Test
+ * @author ะํผฬิช
+ * @date 2020/12/23
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, world!\n");
+}
