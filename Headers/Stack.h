c5a3e0699156cf2e7d36c46c5b630002b08605e3
许继元 <490601115@qq.com>
2021-01-07 15:56:33 +0800

    feat: add passenger stack structure type

---
+++ Headers/Stack.h
@@ -0,0 +1,20 @@
+/**
+ * @filename Stack.h
+ * @description stack structure header file
+ * @author 许继元
+ * @date 2020/12/23
+ */
+
+#ifndef ELEVATORSIMULATION_STACK_H
+#define ELEVATORSIMULATION_STACK_H
+
+#include "ElevatorSimulation.h"
+
+/* 乘客栈结构体类型 */
+typedef struct PassengerStackNode {
+    ElemType *base; /* 栈底指针 */
+    ElemType *top; /* 栈顶指针 */
+    int StackSize; /* 当前已分配存储空间, 以元素为单位 */
+} PassengerStack;
+
+#endif /* ELEVATORSIMULATION_STACK_H */
