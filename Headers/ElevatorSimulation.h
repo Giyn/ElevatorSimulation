bdc2dc3c265cf646b9b1bbd892cefb6ec0337f11
许继元 <490601115@qq.com>
2021-01-07 15:13:39 +0800

    feat: add operation status code

---
+++ Headers/ElevatorSimulation.h
@@ -0,0 +1,19 @@
+/**
+ * @filename ElevatorSimulation.h
+ * @description elevator simulation header file
+ * @author Ðí¼ÌÔª
+ * @date 2020/12/23
+ */
+
+#ifndef ELEVATORSIMULATION_H
+#define ELEVATORSIMULATION_H
+
+/* ²Ù×÷×´Ì¬Âë */
+typedef enum {
+    OK = 1,
+    FAILED = 0,
+    INFEASIBLE = -1,
+    OVERFLOW = -2
+} Status;
+
+#endif /* ELEVATORSIMULATION_H */
